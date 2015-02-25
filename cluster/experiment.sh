bb7520723b4fb405f40610a31ab2f4728c4fba86
Chris Tensmeyer <Christopher.tensmeyer@gmail.com>
2015-02-25 14:16:02 -0700

    final touches 2

--- cluster/experiment.sh
+++ cluster/experiment.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 num_threads=7
-datasets="washpass"
-subsets=""
-initial_clusters=""
-num_seeds=""
-min_pts=""
+datasets="wales_20"
+subsets="5 10"
+initial_clusters="2 3"
+num_seeds="4"
+min_pts="2"
 
-t="1 2 3 4 5 6 7"
+t="1 2"
 
-nice parallel --gnu -j $num_threads ./all.sh {1} {2} ::: $datasets ::: $t
+nice parallel --gnu -j $num_threads ./all.sh {1} {2} {3} {4} {5} {6} ::: $datasets ::: $subsets ::: $initial_clusters ::: $num_seeds ::: $min_pts ::: $t
